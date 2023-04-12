4610fcd3df8703c3ebe0e0fe19aa25a93409dd41
Harsh Choudhary <harshchoudhary271001@gmail.com>
2023-04-12 14:34:09 +0530

    modules commit2

--- cdm_final_project/src/app/app.module.ts
+++ cdm_final_project/src/app/app.module.ts
@@ -3,14 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {AuthComponent} from './modules/auth/auth.component'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
