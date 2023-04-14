1b50ae0d0dc5ada8137212ae3bf9a1ee6de880cd
Sagar Kumar <65843469+Sagar-Kumar-007@users.noreply.github.com>
2023-04-14 12:00:36 +0530

    Update and Delete Icons Added and fixed css of some files

--- cdm_final_project/src/app/modules/dashboard/dashboard/accounts/accounts-dashboard/accounts-dashboard.component.ts
+++ cdm_final_project/src/app/modules/dashboard/dashboard/accounts/accounts-dashboard/accounts-dashboard.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AccountsService } from 'src/app/services/accounts.service';
 
 @Component({
   selector: 'app-accounts-dashboard',
   templateUrl: './accounts-dashboard.component.html',
   styleUrls: ['./accounts-dashboard.component.css']
 })
-export class AccountsDashboardComponent {
+export class AccountsDashboardComponent implements OnInit {
+
+  customerId:number=1;
+  accountsService:AccountsService|null=null;
+  accountsList:any;
+  constructor(accountsService:AccountsService){
+    this.accountsService=accountsService;
+  }
+
+  ngOnInit(){
+    this.accountsService?.accountsList(this.customerId.toString()).subscribe((result)=>{
+      this.accountsList=result;
+    })
+  }
+
   onToggleClick(){
     console.log("Yes");
     let navigation = document.querySelector(".navigation") as HTMLDivElement;
