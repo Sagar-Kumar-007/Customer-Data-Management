bedc09fd62a5660a7074a1b0c3eefd9902cf73ce
Sagar Kumar <65843469+Sagar-Kumar-007@users.noreply.github.com>
2023-04-26 15:05:42 +0530

    revenue added

--- cdm_final_project/src/app/modules/dashboard/dashboard/accounts/accounts.module.ts
+++ cdm_final_project/src/app/modules/dashboard/dashboard/accounts/accounts.module.ts
@@ -35,7 +35,7 @@ import { RouterModule } from '@angular/router';
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyAzfzsRZ4XEwzxiXnjzTybY6TflZnRTeq4',
       libraries: ['places']
-    }),,
+    }),
     RouterModule
   ],
   exports:[
