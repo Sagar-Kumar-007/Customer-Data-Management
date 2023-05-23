ac89f3d0d172b8fccc51bf4305c6bc2e4cf6f23c
Utkarsh <kutkarsh350@gmail.com>
2023-05-23 15:57:35 +0530

    ..

---
+++ DataTrackr API/DataTrackr API/Helpers/UtilityService/EmailBody.cs
@@ -0,0 +1,17 @@
+﻿using DataTrackr_API.Models;
+
+namespace DataTrackr_API.Helpers.UtilityService
+{
+    public static class EmailBody
+    {
+        public static string EmailStringBody(string email,string emailToken)
+        {
+            return $@"
+                    <html>
+                     <head></head>
+                     <body>Security code to reset your password is: {emailToken} </body>
+                    </html>
+                    ";
+        }
+    }
+}
